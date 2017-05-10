099ae18d88df3101d269665cbba8b2846d764dbb
chengyi818 <chengyi818@foxmail.com>
2017-05-10 18:46:26 +0800

    Emacs | Add push to develop branch

--- backup/.spacemacs.d_now/layers/chengyi/config.el
+++ backup/.spacemacs.d_now/layers/chengyi/config.el
@@ -4,13 +4,22 @@
 ;; set coding
 (define-coding-system-alias 'UTF-8 'utf-8)
 
-(defun magit-push-to-gerrit ()
+(defun magit-push-to-gerrit-master ()
   (interactive)
   (magit-git-command "push origin HEAD:refs/for/master" (magit-toplevel)))
+
+(defun magit-push-to-gerrit-develop ()
+  (interactive)
+  (magit-git-command "push origin HEAD:refs/for/develop" (magit-toplevel)))
 
 (with-eval-after-load 'magit
   (progn
     (magit-define-popup-action 'magit-push-popup
       ?g
-      "Push to gerrit"
-      'magit-push-to-gerrit)))
+      "Push to gerrit master"
+      'magit-push-to-gerrit-master)
+    (magit-define-popup-action 'magit-push-popup
+      ?d
+      "Push to gerrit develop"
+      'magit-push-to-gerrit-develop)
+    ))
