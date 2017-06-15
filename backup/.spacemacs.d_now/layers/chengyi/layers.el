b9e542e7c9f072d3d80aab7a878e3e5cc91ff812
chengyi818 <chengyi818@foxmail.com>
2017-06-15 14:56:39 +0800

    Develop | Emacs: update robot layer
    Signed-off-by: chengyi818 <chengyi818@foxmail.com>

--- backup/.spacemacs.d_now/layers/chengyi/layers.el
+++ backup/.spacemacs.d_now/layers/chengyi/layers.el
@@ -1,4 +1,5 @@
 (configuration-layer/declare-layers '(
                                       chengyi_make_header
                                       chengyi_org
+                                      chengyi_robot
                                       ))
---
+++ backup/.spacemacs.d_now/layers/chengyi_robot/config.el
@@ -0,0 +1,2 @@
+;;robot-framework
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . robot-mode))
