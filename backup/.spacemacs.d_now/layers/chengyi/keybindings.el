e7b470ef0738961da5d4be641f0438fbe6ac94a3
chengyi818 <chengyi818@foxmail.com>
2018-02-01 10:19:39 +0800

    Develop | Add remove ^M keybinding

--- backup/.spacemacs.d_now/layers/chengyi/keybindings.el
+++ backup/.spacemacs.d_now/layers/chengyi/keybindings.el
@@ -13,3 +13,12 @@
 ;; Also in visual mode
 (define-key evil-visual-state-map "j" 'evil-next-visual-line)
 (define-key evil-visual-state-map "k" 'evil-previous-visual-line)
+
+;; remove ^M in emacs
+(spacemacs/set-leader-keys "om" 'delete-carrage-returns)
+(defun delete-carrage-returns ()
+  (interactive)
+  (save-excursion
+    (goto-char 0)
+    (while (search-forward "\r" nil :noerror)
+      (replace-match ""))))
