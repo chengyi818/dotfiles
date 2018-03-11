e65491dcd25b4c68b66f380455cd5a5125a85f0d
chengyi818 <chengyi818@foxmail.com>
2018-03-11 22:38:36 +0800

    Update sogou.sh

--- home/.personal_shell/sogou.sh
+++ home/.personal_shell/sogou.sh
@@ -1,9 +1,14 @@
 #/bin/bash
 pkill fcitx && fcitx && sogou-qimpanel && sogou-qimpanel-watchdog
-fcitx-autostart &
 #fcitx-qimpanel
 
-#pidof fcitx | xargs kill
-#pidof sogou-qimpanel | xargs kill
-#nohup fcitx  1>/dev/null 2>/dev/null &
-#nohup sogou-qimpanel  1>/dev/null 2>/dev/null &
+rm -rf ~/.config/SogouPY 
+rm -rf ~/.config/SogouPY.users
+rm -rf ~/.config/sogou-qimpanel
+
+pidof fcitx | xargs kill
+pidof sogou-qimpanel | xargs kill
+nohup fcitx  1>/dev/null 2>/dev/null &
+nohup sogou-qimpanel  1>/dev/null 2>/dev/null &
+
+fcitx-autostart &
