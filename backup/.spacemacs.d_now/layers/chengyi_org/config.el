28b9c5e397adcd5df321846fd74a0daa14e505dd
chengyi818 <chengyi818@foxmail.com>
2019-01-06 23:03:36 +0800

    Bug | Org-mode: Fix org-mode table bug

--- backup/.spacemacs.d_now/layers/chengyi_org/config.el
+++ backup/.spacemacs.d_now/layers/chengyi_org/config.el
@@ -20,7 +20,6 @@
 (setq org-bullets-bullet-list '("🐉" "🐠" "🐬" "🐤"))
 ;; (setq org-bullets-bullet-list '("🐭" "🐮" "🐯" "🐰" "🐲" "🐍" "🐴" "🐑" "🙉" "🐔" "🐶" "🐷"))
 
-;; the %i would copy the selected text into the template
 ;; http://www.howardism.org/Technical/Emacs/journaling-org.html
 ;; add multi-file journal
 (setq org-capture-templates
@@ -38,4 +37,26 @@
                       ("2重要不紧急" . ?2)
                       ("3紧急不重要" . ?3)
                       ("4不重要不紧急" . ?4)
+                      ("Algorithm" . ?a)
+                      ("Review" . ?r)
+                      ("Tip" . ?t)
+                      ("Share" . ?s)
                       ))
+
+
+;; org-table 中英文对齐问题
+;; https://github.com/tumashu/cnfonts
+(set-face-attribute
+ 'default nil
+ :font (font-spec :name "-ADBO-Source Code Pro-ultrabold-italic-normal-*-*-*-*-*-m-0-iso10646-1"
+                  :weight 'normal
+                  :slant 'normal
+                  :size 16))
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font
+   (frame-parameter nil 'font)
+   charset
+   (font-spec :name "-DAMA-Ubuntu Mono-normal-italic-normal-*-*-*-*-*-m-0-iso10646-1"
+              :weight 'normal
+              :slant 'normal
+              :size 17.5)))
